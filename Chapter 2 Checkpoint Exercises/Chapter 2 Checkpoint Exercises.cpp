162d519f5fc4b52aed5d0e15e8e895a3eab08a7e
Tyler Johnston <tdj2957@email.vccs.edu>
2024-09-23 16:52:53 -0400

    I hope it's okay i didn't put my real information but it shows all of the strings correctly.

--- Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
+++ Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
@@ -1,24 +1,14 @@
-// Chapter 2 Checkpoint Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //Tyler Johnston
 //September 23, 2024
 //Chapter 2 Checkpoint Exercises
 //Checkpoint_2-14
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-	char first = 'T', middle = 'D', last = 'J'; //setting variables to be my initials
-	std::cout << first << middle << last << "\n"; //prints my initials with no spaces and leaves an empty line after
+	string full_name = "Tyler Johnston", address = "360 No Scope Ln.", phone_number = "757-123-4567";
+	cout << full_name + " " + address + " " + phone_number;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

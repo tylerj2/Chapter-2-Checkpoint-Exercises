b4c65490a9b11418a4f6183460c37dccf582e1de
Tyler Johnston <tdj2957@email.vccs.edu>
2024-09-23 16:14:30 -0400

    committed only locally i think?

--- Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
+++ Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
@@ -1,11 +1,15 @@
 // Chapter 2 Checkpoint Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Tyler Johnston
+//September 23, 2024
+//Chapter 2 Checkpoint Exercises
+//Checkpoint_2-14
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char first = 'T', middle = 'D', last = 'J'; //setting variables to be my initials
+	std::cout << first << middle << last << "\n"; //prints my initials with no spaces and leaves an empty line after
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu

5e5e886fa5cfab42ed90ac6d80a015b100065260
Tyler Johnston <tdj2957@email.vccs.edu>
2024-09-23 17:10:55 -0400

    yippee! age and weight.

--- Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
+++ Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
@@ -4,10 +4,11 @@
 //Checkpoint_2-14
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
 	int age = 21, weight = 136;
-	cout << 
+	cout << "My age is " << age << " and my weight is " << weight << " pounds.";
 }
